2b1ee4256f9563aca2043c674857c5d6743832b8
timmoth <t.jones@timmoth.com>
2023-04-02 19:14:23 +0100

    removed timestamp

--- handler.js
+++ handler.js
@@ -17,9 +17,15 @@ module.exports.reading = async (event, context, callback) => {
 
   const dataPoint = new Point('weather')
     .tag('uid', body['uid'])
-    .timestamp(body['timestamp'])
-
-
+    .floatField('temperature', body['readings']['temperature'])
+    .floatField('humidity', body['readings']['humidity'])
+    .floatField('pressure', body['readings']['pressure'])
+    .floatField('luminance', body['readings']['luminance'])
+    .floatField('rain', body['readings']['rain'])
+    .floatField('rain_per_second', body['readings']['rain_per_second'])
+    .floatField('wind_direction', body['readings']['wind_direction'])
+    .floatField('wind_speed', body['readings']['wind_speed'])
+    .floatField('voltage', body['readings']['voltage'])
 
   //write data point
   await writeApi.writePoint(dataPoint)
