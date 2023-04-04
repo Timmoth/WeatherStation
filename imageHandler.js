9e14b63fb6fc78d919c3b8b602f73acd438eda74
timmoth <t.jones@timmoth.com>
2023-04-04 23:22:47 +0100

    updated imagehandler

--- imageHandler.js
+++ imageHandler.js
@@ -1,20 +1,55 @@
 'use strict';
-var gm = require('gm').subClass({ imageMagick: true });
-require('gm-base64');
+var PImage = require('pureimage');
+var stream = require('stream');
 
-module.exports.image = (event, context, callback) => {
+const { InfluxDB } = require('@influxdata/influxdb-client')
+//grab environment variables
+const org = process.env.org
+const bucket = process.env.bucket
+const token = process.env.token;
+const url = process.env.url
 
-  gm(200, 30, '#fff')
-    .drawText(10, 20, "Hello, World!")
-    .border(2, 2).borderColor('#ff0000')
-    .toBase64('png', function (err, base64) {
-      const response_success = {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: 'ok'
-        }),
-        isBase64Encoded: false
-      };
-      callback(undefined, response_success)
-    });
+module.exports.image = async (event, context, callback) => {
+
+  //create InfluxDB api client with URL and token, then create Write API for the specific org and bucket
+  const queryApi = await new InfluxDB({ url, token }).getQueryApi(org);
+
+  const fluxQuery = `\
+from(bucket:"${bucket}")\
+|> range(start: -3h)\
+|> filter(fn: (r) => r._measurement == "weather" and r._field == "temperature")\
+|> last()
+`;
+
+  var fontRecord = PImage.registerFont('/var/task/fonts/SourceSansPro-Regular.ttf', 'Source Sans Pro');
+  fontRecord.loadSync();
+
+  var img = PImage.make(400, 200);
+  var ctx = img.getContext('2d');
+  ctx.clearRect(0, 0, img.width, img.height);
+
+  ctx.fillStyle = 'black';
+  ctx.font = "20pt 'Source Sans Pro'";
+
+  let i = 1;
+  for await (const { values, tableMeta } of queryApi.iterateRows(fluxQuery)) {
+    const o = tableMeta.toObject(values)
+    ctx.fillText(`${o._value}°C`, 20, 20 * i++);
+  }
+
+  const passThroughStream = new stream.PassThrough();
+  const pngData = [];
+  passThroughStream.on('data', chunk => pngData.push(chunk));
+  passThroughStream.on('end', () => { });
+  await PImage.encodePNGToStream(img, passThroughStream);
+  let buf = Buffer.concat(pngData);
+
+  callback(undefined, {
+    statusCode: 200,
+    headers: {
+      "content-type": "image/png",
+    },
+    body: buf.toString('base64'),
+    isBase64Encoded: true
+  })
 };
